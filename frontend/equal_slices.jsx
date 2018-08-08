9659535e5ca3c3ac174d9ced0e20b64f439881cd
“Peter <peterwongisnow@gmail.com>
2018-08-08 16:11:08 -0700

    Remove windows in entry file

--- frontend/equal_slices.jsx
+++ frontend/equal_slices.jsx
@@ -4,10 +4,6 @@ import { signup, login, logout} from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
-
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Hellooo world</h1>, root);
 });
