dfafefeb8a2cf1ea36721d098f356458bdd6b250
“Peter <peterwongisnow@gmail.com>
2018-08-08 12:16:52 -0700

    Complete User model and Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    unless logged_in?
+      render json: { base: ['invalid credentials'] }, status: 401
+    end 
+  end
+
 end
