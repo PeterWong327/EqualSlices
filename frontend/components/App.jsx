bb1d00ea0b5d7e3bde932a56f49bb54abc843fe9
“Peter <peterwongisnow@gmail.com>
2018-08-09 14:01:56 -0700

    complete component session form for logging in and signing up a user" git status

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
+import LoginFormContainer from './session_form/login_form_container';
+import SignupFormContainer from './session_form/signup_form_container';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
@@ -7,6 +10,10 @@ const App = () => (
       <h1>EqualSlices</h1>
       <GreetingContainer />
     </header>
+
+    <Route path="/login" component={LoginFormContainer}></Route>
+    <Route path="/signup" component={SignupFormContainer}></Route>
+
   </div>
 );
 
