4f07f894f5b124c68221dbb6521aeebab34cd386
“Peter <peterwongisnow@gmail.com>
2018-08-08 17:56:26 -0700

    Complete front-end user auth sessionReducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,48 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+
+export const login = (user) => dispatch (
+  APIUtil.login(user).then((user) => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+  dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch (
+  APIUtil.logout().then((user) => (
+    dispatch(logoutCurrentUser())
+  ))
+);
+
+export const signup = (user) => dispatch (
+  APIUtil.signup(user).then((user) => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const logoutCurrentUser = () => {
+  return {
+    type: LOGOUT_CURRENT_USER
+  };
+};
+
+export const receiveErrors = errors => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
