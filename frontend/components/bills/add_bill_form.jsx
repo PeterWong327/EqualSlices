59732a6a9ec822525a667c62cccf5b3a33b9c94e
“Peter <peterwongisnow@gmail.com>
2018-09-21 00:43:03 -0700

    Styled add bill modal

--- frontend/components/bills/add_bill_form.jsx
+++ frontend/components/bills/add_bill_form.jsx
@@ -48,66 +48,75 @@ class AddBillForm extends React.Component {
 
 
         <form onSubmit={this.handleSubmit}>
-        This is the bill form
-          <div onClick={this.props.closeModal} ></div>
+          <div className="add-bill-header">Add a Bill</div>
 
-          <label className="add-bill-amount">Bill Amount
+          <div onClick={this.props.closeModal} className="add-bill-x-btn">x</div>
+
+<br></br>
+          <label className="add-bill-amount">$
             <input
               className="add-bill-amount-input"
-              type="text"
+              type="text1"
+              placeholder="0.00"
               value={this.state.bill.amount}
               onChange={this.update('balance')}
               />
           </label>
 
           <br></br>
+          <br></br>
 
-          <label className="add-bill-billee">Friend
+          <label className="add-bill-billee">
             <input
               className="add-bill-billee-input"
-              type="text"
+              type="text1"
+              placeholder="Friend ID"
               value={this.state.bill.bill_receipient_id}
               onChange={this.update('bill_recipient_id')}
               />
           </label>
 
           <br></br>
+          <br></br>
 
-            <label className="add-bill-description">Description
+            <label className="add-bill-description">
               <input
                 className="add-bill-description-input"
-                type="text"
+                type="text1"
+                placeholder="Enter a description"
                 value={this.state.bill.description}
                 onChange={this.update('description')}
                 />
             </label>
 
             <br></br>
+            <br></br>
 
-            <label className="add-bill-date">Date
+            <label className="add-bill-date">
               <input
                 className="add-bill-date-input"
-                type="text"
+                type="date"
+                placeholder="Date"
                 value={this.state.bill.date}
                 onChange={this.update('date')}
                 />
             </label>
 
-          <input
-            className="add-bill-btn"
-            type="submit"
-            value="Save Bill!"
-            onClick={() => {this.props.createBill(this.state.bill)}}
-          />
+            <div className="add-bill-modal-btns">
+              <input
+                className="add-bill-cancel-btn"
+                type="submit"
+                value="Cancel"
+                onClick={this.props.closeModal}
+                />
 
-
-
-          <input
-            className="settle-bill-cancel-btn"
-            type="submit"
-            value="Cancel"
-            onClick={this.props.closeModal}
-            />
+              <input
+                className="add-bill-btn"
+                type="submit"
+                value="Save Bill!"
+                onClick={() => {this.props.createBill(this.state.bill)}}
+              />
+            </div>
 
         </form>
 
